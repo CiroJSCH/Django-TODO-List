d08acef274b304910714d762dfe9ec4b727d516a
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-23 19:36:09 -0300

    feat: get all tasks view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Caca")
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, "tasks.html", {"tasks": tasks})
