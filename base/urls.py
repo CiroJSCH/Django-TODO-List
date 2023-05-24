023dd263d60b3ffa67b1216536bd104267a2dbcb
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-24 11:08:21 -0300

    feat: create task view

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.task_list, name="tasks"),
-    path("task/<int:id>", views.task_detail, name="task_detail")
+    path("task/<int:id>", views.task_detail, name="task_detail"),
+    path("task/create", views.task_create, name="task_create")
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import CreateTaskForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,3 +14,23 @@ def task_list(request):
 def task_detail(request, id):
     task = Task.objects.get(pk=id)
     return render(request, 'task_detail.html', {'task': task})
+
+
+def task_create(request):
+    if request.method == "POST":
+        form = CreateTaskForm(request.POST)
+        if form.is_valid():
+            # Hardcodeado por el momento
+            user = User(id=1)
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            completed = form.cleaned_data["completed"]
+
+            task = Task(user=user, title=title, description=description,
+                        completed=completed)
+            task.save()
+            return redirect("tasks")
+        else:
+            return render(request, "task_create.html", {"form": form, 'error': 'Bad data passed in. Try again.'})
+    else:
+        return render(request, "task_create.html", {"form": CreateTaskForm()})
