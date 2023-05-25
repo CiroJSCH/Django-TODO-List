9c3886f1dc52a8a2c07e083b41baac58da8af48f
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-24 21:47:22 -0300

    feat: login and logout urls

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.task_list, name="tasks"),
+    path("login", views.signin, name="login"),
+    path("logout", views.signout, name="logout"),
     path("task/<int:id>", views.task_detail, name="task_detail"),
     path("task/create", views.task_create, name="task_create"),
     path("task/update/<int:id>", views.task_update, name="task_update"),
-    path("task/delete/<int:id>", views.task_delete, name="task_delete")
+    path("task/delete/<int:id>", views.task_delete, name="task_delete"),
 ]
