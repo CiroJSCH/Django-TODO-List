b428f33326e1690f3ce4e58dfbe4d1d42538663c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-24 21:46:47 -0300

    feat: login form

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Task
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'completed']
+
+
+class SignInForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
